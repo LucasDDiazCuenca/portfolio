0c4588eac53ba64fcb6a0c5c6a321068f9c08c48
LucasDDiazCuenca <lucasDamian30@gmail.com>
2023-09-14 04:11:33 +0100

    install tailwind; start project and implement floating spheres

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import FloatingBubbles from "../components/Bubbles";
+
+export default function Home() {
+    return<div className="border-example w-screen h-screen">
+        <FloatingBubbles></FloatingBubbles>
+    </div>
+}
